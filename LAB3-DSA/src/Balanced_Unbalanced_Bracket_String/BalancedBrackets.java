8c162ee53eddfb83acf83ee7cac60d9b074d489f
Sangal <shsangal@deloitte.com>
2021-10-10 17:04:18 +0530

    DSA_LAB_Solution_Shivam_Sangal

---
+++ LAB3-DSA/src/Balanced_Unbalanced_Bracket_String/BalancedBrackets.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package Balanced_Unbalanced_Bracket_String;
+
+import java.util.*;
+
+/**
+ * @author Shivam Sangal
+ *
+ */
+public class BalancedBrackets {
+	 
+    // function to check if brackets are balanced
+    static boolean areBracketsBalanced(String expr)
+    {
+        Deque<Character> stack
+            = new ArrayDeque<Character>();
+ 
+        // Traversing the Expression
+        for (int i = 0; i < expr.length(); i++)
+        {
+            char x = expr.charAt(i);
+ 
+            if (x == '(' || x == '[' || x == '{')
+            {
+                // Push the element in the stack
+                stack.push(x);
+                continue;
+            }
+ 
+            // If current character is not opening
+            // bracket, then it must be closing. So stack
+            // cannot be empty at this point.
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+            case ')':
+                check = stack.pop();
+                if (check == '{' || check == '[')
+                    return false;
+                break;
+ 
+            case '}':
+                check = stack.pop();
+                if (check == '(' || check == '[')
+                    return false;
+                break;
+ 
+            case ']':
+                check = stack.pop();
+                if (check == '(' || check == '{')
+                    return false;
+                break;
+            }
+        }
+ 
+        // Check Empty Stack
+        return (stack.isEmpty());
+    }
+ 
+    // Driver code
+    public static void main(String[] args)
+    {
+               
+        Scanner sc = new Scanner(System.in);
+    	System.out.println("enter a string with '(', '{' or '['");
+    	String expr = sc.nextLine();
+    	 // Function call
+        if (areBracketsBalanced(expr))
+            System.out.println("Balanced ");
+        else
+            System.out.println("Not Balanced ");
+        
+    }
+}
---
+++ LAB3-DSA/src/Longest_Path_Binary_Tree/Longest_Path_BinaryTree.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package Longest_Path_Binary_Tree;
+
+/**
+ * @author shsangal
+ *
+ */
+
+	import java.io.*;
+	import java.util.ArrayList;
+	 
+	class Longest_Path_BinaryTree{
+	     
+	// Binary tree node
+	static class Node
+	{
+	    Node left;
+	    Node right;
+	    int data;
+	};
+	 
+	// Function to create a new
+	// Binary node
+	static Node newNode(int data)
+	{
+	    Node temp = new Node();
+	 
+	    temp.data = data;
+	    temp.left = null;
+	    temp.right = null;
+	 
+	    return temp;
+	}
+	 
+	// Function to find and return the
+	// longest path
+	public static ArrayList<Integer> longestPath(Node root)
+	{
+	     
+	    // If root is null means there
+	    // is no binary tree so
+	    // return a empty vector
+	    if(root == null)
+	    {
+	        ArrayList<Integer> output = new ArrayList<>();
+	        return output;
+	    }
+	     
+	    // Recursive call on root.right
+	    ArrayList<Integer> right = longestPath(root.right);
+	     
+	    // Recursive call on root.left
+	    ArrayList<Integer> left = longestPath(root.left);
+	     
+	    // Compare the size of the two ArrayList
+	    // and insert current node accordingly
+	    if(right.size() < left.size())
+	    {
+	        left.add(root.data);
+	    }
+	    else
+	    {
+	        right.add(root.data);
+	    }
+	     
+	    // Return the appropriate ArrayList
+	    return (left.size() >
+	            right.size() ? left :right);
+	}
+	 
+	// Driver Code
+	public static void main(String[] args)
+	{
+		Node root = newNode(100);
+		root.left = newNode(20);
+		root.right = newNode(130);
+	  	root.right.left = newNode(110);
+	    root.right.right = newNode(140);
+		root.left.left = newNode(10);
+		root.left.right = newNode(50);
+		root.left.left.left = newNode(5);
+	     
+	    ArrayList<Integer> output = longestPath(root);
+	    int n = output.size();
+	     
+	    System.out.print(output.get(n - 1));
+	    for(int i = n - 2; i >= 0; i--)
+	    {
+	        System.out.print(" -> " + output.get(i));
+	    }
+	}
+	}
